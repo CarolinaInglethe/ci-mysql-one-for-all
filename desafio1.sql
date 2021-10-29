25a8e50c572f300951ec337672e60c6cfd537bd4
Carol_Inglethe <carolinainglethe@gmail.com>
2021-10-29 19:40:25 -0300

    Desafio 1 - inserindo Valores na tabela

--- desafio1.sql
+++ desafio1.sql
@@ -51,3 +51,82 @@ CREATE TABLE historico_reproducoes(
   FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuarios (usuario_id),
   FOREIGN KEY (musica_id) REFERENCES SpotifyClone.musicas (musica_id)
 ) engine = InnoDB;
+
+
+INSERT INTO SpotifyClone.artistas (`nome_artista`)
+VALUES 
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+
+
+INSERT INTO SpotifyClone.albums (`nome_album`, `artista_id`)
+VALUES 
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
+  
+  INSERT INTO SpotifyClone.musicas (`nome_musica`, `album_id`)
+  VALUES
+    ('Soul For US', 1),
+    ('Reflections Of Magic', 1),
+    ('Dance With Her Own', 1),
+    ('Troubles Of My Inner Fire', 2),
+    ('Time FireWorks', 2),
+    ('Magic Circus', 3),
+    ('Honey,So Do I', 3),
+    ('Sweetie, Let\'s Go Wild', 3),
+    ('She Knows', 3),
+    ('Fantasy For ME',4),
+    ('Celebration Of More', 4),
+    ('Rock His Everything', 4),
+    ('Home Forever', 4),
+    ('Diamond Power', 4),
+    ('Honey, Let\s Be Silly', 4),
+    ('Thang Of Thunder',5),
+    ('Words Of Her Life', 5),
+    ('Without My Streets', 5);
+
+INSERT INTO SpotifyClone.planos (`nome_plano`, `valor_plano`)
+VALUES 
+  ('gratuito', 0),
+  ('familiar', 7.99),
+  ('universitário', 5.99);
+  
+INSERT INTO SpotifyClone.usuarios (`nome_usuario`,`idade`, `plano_id`)
+VALUES 
+  ('Thati', 23, 1),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
+  ('Roger', 45, 1);
+  
+INSERT INTO SpotifyClone.seguindo_artistas (`usuario_id`,`artista_id`)
+VALUES
+  (1, 1),
+  (1, 4),
+  (1, 3),
+  (2, 1),
+  (2, 3),
+  (3, 2),
+  (3, 1),
+  (4, 4);
+  
+INSERT INTO SpotifyClone.historico_reproducoes (`usuario_id`, `musica_id`)
+VALUES 
+  (1, 1),
+  (1, 6),
+  (1, 14),
+  (1, 16),
+  (2, 13),
+  (2, 17),
+  (2, 2),
+  (2, 15),
+  (3, 4),
+  (3, 16),
+  (3, 6),
+  (4, 3),
+  (4, 18),
+  (4, 11);
