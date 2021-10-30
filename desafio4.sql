63a5304f1fc725e4b20a7d12df3aa1a66cf16fc0
Carol_Inglethe <carolinainglethe@gmail.com>
2021-10-29 22:06:29 -0300

    Desafio 4 - artistas e sua quantidade de seguidores

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+ CREATE VIEW top_3_artistas AS
+   SELECT 
+     A.`nome_artista` AS `artista`, 
+     COUNT(SA.`usuario_id`) AS `seguidores`
+   FROM SpotifyClone.seguindo_artistas AS SA
+   JOIN SpotifyClone.artistas AS A  ON A.`artista_id` = SA.`artista_id`
+   GROUP BY `artista`
+   ORDER BY `seguidores` DESC, `artista`;
